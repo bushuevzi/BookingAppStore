17c4e562458de1b776fe94418e668e4f4cc16eda
bushuevzi@mail.ru <bushuevzi@mail.ru>
2019-03-03 22:34:32 +0300

    сделаны модели, сделаны контроллеры, сделана база через EF

--- BookingAppStore/Controllers/HomeController.cs
+++ BookingAppStore/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using BookingAppStore.Models;
+using BookingAppStore.Util;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,7 +19,86 @@ namespace BookingAppStore.Controllers
             var books = db.Books;
             //передаем ее в представление
             ViewBag.Books = books;
+            HttpContext.Response.Cookies["token"].Value = "user-1234567890";
             return View();
+        }
+        public string GetToken()
+        {
+            string id = HttpContext.Request.Cookies["token"].Value;
+            return id.ToString();
+        }
+
+        public ActionResult GetBook()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public string GetBook(string title, string author)
+        {
+            return $"{title} {author}";
+        }
+
+        public string GetContext()
+        {
+            string browser = HttpContext.Request.Browser.Browser;
+            string userAgent = HttpContext.Request.UserAgent;
+            string url = HttpContext.Request.RawUrl;
+            string ip = HttpContext.Request.UserHostAddress;
+            string referrer = HttpContext.Request.UrlReferrer == null ? "" : HttpContext.Request.UrlReferrer.AbsoluteUri;
+            return $"<p>Browser: {browser}</p><p>User-Agent: {userAgent}</p><p>Url запроса: {url}</p>" +
+                $"<p>Реферер: {referrer}</p><p>IP-адрес: {ip}</p>";
+        }
+
+        public FilePathResult GetFile()
+        {
+            //путь к файлу
+            string filePath = Server.MapPath("~/Files/test.pdf");
+            //тип файла -- content-type
+            string fileType = "application/octet-stream";
+            //Имя файла (необязательно)
+            string fileName = "test.pdf";
+
+            return File(filePath, fileType, fileName);
+        }
+
+
+        public FileResult GetBytes()
+        {
+            string filePath = Server.MapPath("~/Files/test.pdf");
+            byte[] mas = System.IO.File.ReadAllBytes(filePath);
+            string fileType = "application/octet-stream";
+            string fileName = "test.pdf";
+            return File(mas, fileType, fileName);
+        }
+
+        public ActionResult GetHtml()
+        {
+            return new HtmlResult("<h2>Привет мир</h2>");
+        }
+
+        public ActionResult GetImage()
+        {
+            string path = "../Content/Images/youtube.png";
+            return new ImageResult(path);
+
+        }
+
+        public ActionResult Buy(int id)
+        {
+            ViewBag.BookId = id;
+            return View();
+        }
+
+
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+            db.Purchases.Add(purchase);
+            db.SaveChanges();
+            return $"Спасибо, {purchase.Person} за покупку!";
         }
 
         public ActionResult About()
---
+++ BookingAppStore/Controllers/MyController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace BookingAppStore2.Controllers
+{
+    public class MyController : IController
+    {
+        public void Execute(RequestContext requestContext)
+        {
+            string ip = requestContext.HttpContext.Request.UserHostAddress;
+            var response = requestContext.HttpContext.Response;
+            response.Write($"<h2>Ваш IP-адресс: {ip}</h2>");
+        }
+    }
+}
